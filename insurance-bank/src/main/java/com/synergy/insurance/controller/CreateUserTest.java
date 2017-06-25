73e99b5d8888358b792b5957e242a37af3ce3452
xxxPeace <rtan9@ucsc.edu>
2017-06-25 04:13:11 -0700

    Add Login Entity

--- insurance-bank/src/main/java/com/synergy/insurance/controller/CreateUserTest.java
+++ insurance-bank/src/main/java/com/synergy/insurance/controller/CreateUserTest.java
@@ -10,14 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.synergy.insurance.dao.LoginDao;
 import com.synergy.insurance.model.AnthoritiesEntity;
+import com.synergy.insurance.model.CustomerEntity;
 import com.synergy.insurance.model.LoginEntity;
 
 @Controller
 public class CreateUserTest {
+
 	@Autowired
-	@Qualifier("hibernateTemplate")
-	private HibernateTemplate hibernateTemplate;
+	@Qualifier("loginDao")
+	private LoginDao loginDao;
 	
 	//test will return object as Json format
 	@RequestMapping(value = "usertest",method=RequestMethod.GET)
@@ -36,16 +39,34 @@ public class CreateUserTest {
 	@ResponseBody public LoginEntity userSaveTest() {
 		System.out.println("user save test");
 		LoginEntity user1 = new LoginEntity();
-		user1.setEmail("rockx@qq.com");
+		user1.setEmail("rtandsadsa2@qq.com");
 		user1.setPassword("123");
 		user1.setEnabled(true);
 		
-		AnthoritiesEntity anth = new AnthoritiesEntity();
-		anth.setAnthority("ROLE_USER");
-		anth.setUser(user1);
-		//hibernateTemplate.setCheckWriteOperations(false);
-		hibernateTemplate.save(user1);
-		hibernateTemplate.save(anth);
+		//loginDao.createManager(user1);
+		loginDao.createCustomer(user1);
+		//loginDao.createThirdPraty(user1);
 		return user1;
+	}
+	
+	@RequestMapping(value = "getUserByEmail",method=RequestMethod.GET)
+	@ResponseBody public LoginEntity getUserByEmail() {
+		System.out.println("getUserByEmail");
+		LoginEntity user = loginDao.getUserByEmail("rtan11222@qq.com");
+		return user;
+	}
+	
+	@RequestMapping(value = "getCustomerByEmail",method=RequestMethod.GET)
+	@ResponseBody public CustomerEntity getCustomerByEmail() {
+		System.out.println("getCustomerByEmail");
+		CustomerEntity customer = loginDao.getCustomerByEmail("rtandsadsa2@qq.com");
+		return customer;
+	}
+	
+	@Transactional
+	@RequestMapping(value = "deleteCustomerByEmail",method=RequestMethod.GET)
+	@ResponseBody public void deleteCustomerByEmail() {
+		System.out.println("deleteCustomerByEmail");
+		loginDao.deleteCustomerByEmail("rtandsadsa2@qq.com");
 	}
 }
