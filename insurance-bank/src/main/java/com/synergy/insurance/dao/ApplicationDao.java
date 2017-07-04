d8e1b9690be280e17adf9a93650faa857c192473
xxxPeace <rtan9@ucsc.edu>
2017-07-04 03:59:07 -0700

    commit

---
+++ insurance-bank/src/main/java/com/synergy/insurance/dao/ApplicationDao.java
@@ -0,0 +1,45 @@
+package com.synergy.insurance.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Service;
+
+import com.synergy.insurance.model.Application;
+
+@Service("applicationDao")
+public class ApplicationDao {
+
+	@Autowired
+	@Qualifier("hibernateTemplate")
+	private HibernateTemplate hibernateTemplate;
+	
+	public void createApplication(Application app){
+		hibernateTemplate.save(app);
+	}
+	
+	public void updateApplication(Application app){
+
+	}
+	public List<Application> getApplications(){
+		List<Application> listApplications= (List<Application>) hibernateTemplate.find("from Application");		
+		return listApplications;
+		
+	}
+	public Application getApplicationByID(int id){
+		List<Application> list = (List<Application>) hibernateTemplate.find("from Application where applicationId = ?",id);
+		 if (list.size() != 0) {
+		    return list.get(0);
+		 } else {
+		    return new Application();
+		 }
+	}
+	
+	public List<Application> getApplicationByStatus(String status){
+		List<Application> listApplications = (List<Application>) hibernateTemplate.find("from Application where status = ?",status);
+		return listApplications;
+	}
+	
+}
